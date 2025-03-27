2749e77304274fa28db868c3730677952eb7173c
Guilherme Dias <guilhermedias.ferreira@hotmail.com>
2025-03-26 21:47:54 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,5 @@ module.exports = defineConfig({
   viewportHeight: 880, //860
   viewportWidth: 1280, //410
   e2e: {},
-  projectId: "3jb85c"
-
-  // CYPRESS_RECORD_KEY = fc7f4674-4c42-4e58-b972-0ddcee0a253f
+  projectId: "3ioozy"
 })
